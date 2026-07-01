f2d9ac630867ea3be131b2e62e749e37ca22b5d7
vikash-kun <12345vikash23@gmail.com>
2026-07-01 13:01:09 +0530

    main.py updated

---
+++ main.py
@@ -0,0 +1,10 @@
+def add_student():
+    name = input("Enter your name :")
+    age = int(input("Enter your age :"))
+    course = input("Enter your course :")
+    print("\nStudent Details")
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+    print(f"Course: {course}")
+add_student()
+
