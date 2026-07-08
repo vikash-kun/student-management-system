11a39a05fd8320573f7e4e5ee209a19dd9ba3e1b
vikash-kun <12345vikash23@gmail.com>
2026-07-08 13:05:34 +0530

    Day 9: Added input validation and exception handling

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from database import insert_student, view_students, search_student, update_stude
 
 def add_student():
     name = input("Enter your name: ")
-    age = int(input("Enter your age: "))
+    if name.strip() == "":
+     print("Name cannot be empty.")
+     return
+    try:    
+     age = int(input("Enter your age: "))
+    except ValueError:
+      print("Invalid age! Please enter a number.")
+      return   
     course = input("Enter your course: ")
-
+    if course.strip() == "":
+     print("Course cannot be empty.")
+     return
     insert_student(name, age, course)
 
     print("\nStudent Details")
@@ -24,29 +33,40 @@ def menu():
 
 while True:
     menu()
-    choice = input("Enter your choice: ")
-
-    if choice == "1":
+    try:    
+     choice = int(input("Enter your choice: "))
+    except ValueError:
+      print("Invalid choice! Please enter a number.")
+      continue
+    if choice == 1:
         add_student()
-    elif choice == "2":
+    elif choice == 2:
         view_students()
-    elif choice == "3" :
+    elif choice == 3 :
         name = input("Enter student name: ")
         search_student(name) 
-    elif choice == "4":
-        student_id = int(input("Enter Student ID: "))
-        age = int(input("Enter New Age: "))
+    elif choice == 4:
+        try:
+         student_id = int(input("Enter Student ID: "))
+         age = int(input("Enter New Age: "))
+        except ValueError:
+         print("Student ID and Age must be numbers.")
+         continue
         course = input("Enter New Course: ")  
         update_student(student_id, age, course)
         print("Student updated successfully!") 
-    elif choice == "5":
-        student_id = int(input("Enter id you want to delete"))  
+    elif choice == 5:
+        try:
+         student_id = int(input("Enter Student ID to delete: "))
+        except ValueError:
+         print("Student ID must be a number.")
+         continue
 
         delete_student(student_id)
 
         print("Student deleted successfully!")
-    elif choice == "6":
+    elif choice == 6:
      print("Thank you for using Student Management System.")
      break
     else :
-        print("Invalid choice. Please try again.")
+      print("Invalid choice. Please try again.")
